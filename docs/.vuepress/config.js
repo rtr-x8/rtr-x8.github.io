e5674ed143e35fc7b602db1e459a04dfbb1aaf3f
rtr-x8 <ryu.2.24@hotmail.co.jp>
2022-02-12 22:12:08 +0900

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,21 @@
 module.exports = {
   port: 8888,
   lang: 'ja',
+  title: 'RM',
+  description: 'RMによる忘備録。学び、思考、未来について。',
   locales: {
     '/': {
       lang: 'ja'
     }
   },
+  head: [
+    ['link', { rel: 'icon', href: '/images/favicon.ico' }],
+    ['meta', { name: 'og:image', content: 'https://rtr-x8.github.io/images/hero400x400.jpg' }],
+  ],
   themeConfig: {
     nav: [
-      { text: 'Twitter', link: 'https://twitter.com/rtr_x8', rel: "noopener noreferrer" },
-      { text: 'Note', link: 'https://note.com/rtr_x8', rel: "noopener noreferrer" },
-      { text: 'Github', link: 'https://github.com/rtr-x8/', rel: "noopener noreferrer" },
+      { text: 'Twitter', link: 'https://twitter.com/rtr_x8', rel: "noopener noreferrer", target: '_blank' },
+      { text: 'Github', link: 'https://github.com/rtr-x8/', rel: "noopener noreferrer", target: '_blank' },
     ],
     lastUpdated: 'Last Updated',
     sidebar: 'auto',
