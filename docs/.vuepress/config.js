951018120a51c8f124bc9ac5aea21060a0dca2c4
rtr-x8 <ryu.2.24@hotmail.co.jp>
2022-02-13 15:13:27 +0900

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,66 @@
+const siteName = 'RM logs'
+const productionUrl = 'https://rtr-x8.github.io'
+
 module.exports = {
-  port: 8888,
-  lang: 'ja',
-  title: 'RM',
-  description: 'RMによる忘備録。学び、思考、未来について。',
-  locales: {
-    '/': {
-      lang: 'ja'
-    }
-  },
-  head: [
-    ['link', { rel: 'icon', href: '/images/favicon.ico' }],
-    ['meta', { name: 'og:image', content: 'https://rtr-x8.github.io/images/hero400x400.jpg' }],
-  ],
-  themeConfig: {
-    nav: [
-      { text: 'Twitter', link: 'https://twitter.com/rtr_x8', rel: "noopener noreferrer", target: '_blank' },
-      { text: 'Github', link: 'https://github.com/rtr-x8/', rel: "noopener noreferrer", target: '_blank' },
+    port: 8888,
+    lang: 'ja',
+    title: siteName,
+    description: 'RMによる忘備録。学び、思考、未来について。',
+    locales: {
+        '/': {
+            lang: 'ja'
+        }
+    },
+    head: [
+        ['link', {rel: 'icon', href: '/images/favicon.ico'}],
+        ['meta', {
+            name: 'og:image',
+            content: productionUrl + '/images/hero400x400.jpg'
+        }],
     ],
-    lastUpdated: 'Last Updated',
-    sidebar: 'auto',
-  },
+    themeConfig: {
+        nav: [
+            {
+                text: 'Twitter',
+                link: 'https://twitter.com/rtr_x8',
+                rel: "noopener noreferrer",
+                target: '_blank'
+            },
+            {
+                text: 'Github',
+                link: 'https://github.com/rtr-x8/',
+                rel: "noopener noreferrer",
+                target: '_blank'
+            },
+        ],
+        lastUpdated: 'Last Updated',
+        sidebar: 'auto',
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': 'public/static'
+            }
+        }
+    },
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-49716279-4'
+            }
+        ], [
+            'vuepress-plugin-smooth-scroll'
+        ], [
+            'autometa',
+            {
+                site: {
+                    name: siteName,
+                    twitter: 'rtr_x8',
+                },
+                canonical_base: productionUrl
+            }
+        ],
+        ['@vuepress/nprogress']
+    ],
 };
